5fb1b4b52c9e45a9f2c331d598169fb2b938503b
sarath <sarathchandh1428@gmail.com>
2023-06-23 12:29:29 +0530

    using \n and end

--- InputOutput/Output/PrintStatements.py
+++ InputOutput/Output/PrintStatements.py
@@ -1,3 +1,7 @@
 print('hello!!!')
 print("hello!!!")
 print('''HELLO!!!!''')
+
+print('hey there \nhow are you doing')#we used \n cause it will help us print in diff line
+print('hey',end=' ')
+print('hello',end='')
