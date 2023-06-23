4a85ad42324565fd745d4cae6847618b6d6c1053
sarath <sarathchandh1428@gmail.com>
2023-06-23 10:35:29 +0530

    cleaning up the repository

--- first10NaturalNumbers.py
+++
@@ -1,9 +0,0 @@
-class NaturalNumbers:
-    def __init__(self,Num):
-        self.Num=Num
-    def Logic(self):
-        for x in range(1,self.Num+1):
-            print(x,end=" ")
-Range=int(input("enter the number you want to print natural numbers upto : "))
-obj=NaturalNumbers(Range)
-obj.Logic()
