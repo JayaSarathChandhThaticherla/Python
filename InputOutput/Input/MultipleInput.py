c4ce47a88bd4b3563287cbd71bbc3064db5f2f73
sarath <sarathchandh1428@gmail.com>
2023-06-23 11:58:56 +0530

    Multiple input program added

---
+++ InputOutput/Input/MultipleInput.py
@@ -0,0 +1,6 @@
+def Inp1():
+    x, y = input("Enter two values: ").split()
+    print("Number of boys: ", x)
+    print("Number of girls: ", y)
+    print(x+y)
+Inp1()
--- InputOutput/Input/SingleInput.py
+++ InputOutput/Input/SingleInput.py
@@ -1,2 +1,15 @@
-A=input('sarath')
-print(A)
+def print1():
+    a=20
+    print(a)
+# print1()
+
+def print2():
+    a=20
+    b=30
+    print(a+b)
+# print2()
+
+def print3():
+    a=input()
+    print(a)
+print3()
